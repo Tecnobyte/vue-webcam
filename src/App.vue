7c393c7ddcb943ec94c519de53cbc06d08798930
gerardo <gerardochale01@gmail.com>
2021-04-10 13:03:01 -0500

    modificacion componente

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <WebCamComponent></WebCamComponent>
+  <div id="app">
+    <div>
+      <button @click="iniciar()">iniciar camara</button>
+      <button @click="stop_webcam()">detener camara</button>
+      <button @click="tomar_foto()">tomar foto</button>
+    </div>
+
+    <div>
+      <select @change="seleccionarCamara(dispositivo)" v-model="dispositivo">
+        <option :value="null" disabled hidden selected>SELECCIONE UN DISPOSITIVO</option>
+        <option v-for="webcam of dispositivos" :key="webcam.deviceId" :value="webcam.deviceId">{{webcam.label}}</option>
+      </select>
+    </div>
+
+    <div>
+      <video style="width: 100%" ref="webcam" id="video" :poster="poster" playsinline autoplay></video>
+    </div>
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import WebCamComponent from './components/webcam.vue';
 
+import WebCamComponent from './components/webcam.vue';
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-    WebCamComponent
-    // 'webcam': WebCamComponent
+  extends: WebCamComponent,
+  props: {
+      width: {
+          type: Object,
+          default: function(){ return { min: 1024, ideal: 1280, max: 1920 } }
+      },
+      height: {
+          type: Object,
+          default: function(){ return  { min: 576, ideal: 720, max: 1080 } }
+      },
+      format: {type: String, default: "image/png"},
+      poster: {
+          type: String,
+          default: './../assets/static.git'
+      },
+      audio: {type: Boolean, default: false},        
+  }, data() {
+    return {
+      dispositivos: [],
+      dispositivo: null,
+    };
+  }, methods:{
+    async iniciar(){
+      this.dispositivos = await this.get_devices();
+      if (this.dispositivos.length > 0){
+        this.dispositivo = this.dispositivos[0].deviceId
+        const config = { 
+          video: {
+              width: this.width,
+              height: this.height,
+              deviceId: this.dispositivo 
+          }, audio: this.audio
+        }
+        this.play(config);
+      }else{
+        alert('no existe una camara en el dispositivo');
+      }
+    },
+    tomar_foto(){
+      let b = this.get_base64();
+    }
+
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
 }
 </style>
--- src/components/webcam.vue
+++ src/components/webcam.vue
@@ -1,22 +1,8 @@
 <script>
+    require('babel-polyfill');
     export default {
         name: 'WebCamComponent',
-        props: {
-            width: {
-                type: Object,
-                default: function(){ return { min: 1024, ideal: 1280, max: 1920 } }
-            },
-            height: {
-                type: Object,
-                default: function(){ return  { min: 576, ideal: 720, max: 1080 } }
-            },
-            format: {type: String, default: "image/png"},
-            poster: {
-                type: String,
-                default: './../assets/static.git'
-            },
-            audio: {type: Boolean, default: false},        
-        }, methods: {
+        methods: {
             // obtengo todos los dispositovos que sean tipo video
             get_devices: async function(){
                 try{
@@ -24,11 +10,10 @@
                     if(!tieneSoporteUserMedia()){
                         throw "Parece que tu navegador no soporta esta caracteristica.";
                     }
-
                     // get device 
                     const lista_dispositivos = await navigator.mediaDevices.enumerateDevices();
                     let dispositivos = lista_dispositivos.filter( dispositivo => dispositivo.kind == 'videoinput' );
-                    this.$emit("devices", dispositivos)
+                    return dispositivos;
                 }catch(error){
                     console.error(error)
                 }
@@ -49,34 +34,21 @@
                 this.$refs.webcam.requestPictureInPicture();
             },
             // inicio los parametros de la webcam
-            play: async function(idDeDispositivo) {
-                let media = await navigator.mediaDevices.getUserMedia({ 
-                    video: {
-                        width: { min: 1024, ideal: 1280, max: 1920 },
-                        height: { min: 576, ideal: 720, max: 1080 },
-                        deviceId: idDeDispositivo 
-                    },
-                    audio: this.audio
-                });
+            play: async function(config) {
+                let media = await navigator.mediaDevices.getUserMedia(config);
                 this.$refs.webcam.srcObject = media;
                 this.$refs.webcam.play();
-
-                // if (this.dispositivoSeleccionado == ''){
-                //     this.lista_dispositivos = await this.buscar_dispositivos();
-                //     this.dispositivoSeleccionado = this.lista_dispositivos[0].deviceId; //selecciono al primero
-                // }
             },
-            // obtengo la base64 de la imagen
+            // toma la foto y la convierte en imagen
             get_base64: function(){
+                console.log(this.format);
                 let resolution = this.$refs.webcam;
                 let canvas = document.createElement('canvas');
                 canvas.width = resolution.videoWidth;
                 canvas.height = resolution.videoHeight;
-                // let context = canvas.getContext('2d').drawImage(resolution, 0, 0, resolution.videoWidth, resolution.videoHeight);
-                // this.imagen = canvas.toDataURL(this.format);
-                // this.imagen = canvas.getContext('2d').drawImage(resolution, 0, 0, resolution.videoWidth, resolution.videoHeight).toDataURL(this.format)
-                let imagen = canvas.getContext('2d').drawImage(resolution, 0, 0, resolution.videoWidth, resolution.videoHeight).toDataURL(this.format)
-                this.$emit('canvas', imagen );
+                let context = canvas.getContext('2d').drawImage(resolution, 0, 0, resolution.videoWidth, resolution.videoHeight);
+                let imagen = canvas.toDataURL(this.format)
+                return imagen;
             },
             // convierto el base 64 en una archivo imagen
             conversion_file: function(base64){
@@ -84,47 +56,25 @@
                 let b64 = base64.split(",")[1];
                 let imagen = this.b64toBlob(b64, this.format)
                 let file = new File( [imagen], 'rostro.png', { type: this.format }); // convierto el blob en un archivo png
-                this.$emit('file', file);
+                return file;
             },
             // convierto la cadena base64 de canvas a tipo blob png
             b64toBlob: function(b64Data, contentType, sliceSize) {
                 contentType = contentType || '';
                 sliceSize = sliceSize || 512;
-
                 var byteCharacters = atob(b64Data);
                 var byteArrays = [];
-
                 for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
                     var slice = byteCharacters.slice(offset, offset + sliceSize);
-
                     var byteNumbers = new Array(slice.length);
                     for (var i = 0; i < slice.length; i++) {
                         byteNumbers[i] = slice.charCodeAt(i);
                     }
-
                     var byteArray = new Uint8Array(byteNumbers);
-
                     byteArrays.push(byteArray);
                 }
-
             return new Blob(byteArrays, {type: contentType});
             },
-
-        },render: function(createElement){
-            console.log(createElement)
-            return createElement('video', {
-                ref: 'webcam',
-                style: {
-                    width: '100%'
-                },
-                attrs: {
-                    id: 'webcam',
-                    poster: this.poster,
-                },
-                // on: {
-                //     change: 
-                // }
-            })
         }
     }
 </script>
