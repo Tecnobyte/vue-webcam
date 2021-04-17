3218b5932fef93720b632b615d82d4a0e773239d
tecnobyte <37431521+Tecnobyte@users.noreply.github.com>
2021-04-16 23:55:35 -0500

    tecnobyte: 16 abril 2021 -> modificacion main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import WebCam from './App.vue';
 
-// export default WebCam;
-new Vue({
-  el: '#app',
-  render: h => h(WebCam),
-});
+export default WebCam;
+// new Vue({
+//   el: '#app',
+//   render: h => h(WebCam),
+// });
